8bedf5ba402ddf25df1b52ba11bef057ce06f4db
Jamal Elmir <blankfragz@gmail.com>
2025-02-08 03:47:54 +1100

    Deploy using GitHub Actions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/elementsim/',
   plugins: [react()],
   server: {
     fs: {
-      allow: ['..'], // Allow accessing files in parent directories (e.g., backend/pkg)
+      allow: ['..'], 
     },
   },
 });
